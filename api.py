617df00854850d5bf8249cd95810446f3aff0300
a1-richmond <dev@unigratedpartners.com>
2025-03-30 01:48:50 -0400

    See line 25-29

--- api.py
+++ api.py
@@ -21,7 +21,25 @@ def create_client_session(api_key=""):
 def update_access_token(client:Client, api_key):
     ui.notify(f'Setting Access Token: {api_key}')
     client.set_bearer_token(api_key)
+
+# Patch request to API to update display_order
+# Query/Path/Request Body arguments are already split automatically so there's no need to separate them 
+# Converted the datatype on purpose to test the API clients validate_and_convert function
+# https://github.com/unigrated-solutions/clio-api-python-client/blob/main/classes/base.py
+# TODO: Update API client to convert Literal capitalization mismatches so that Matter/matter or Contact/contact get converted automatically
+def update_custom_field_display_order(client:Client, field_id, new_position):
+    field_id=str(field_id)
+    new_position=str(new_position)
+    try:
+        response = {"Success": True}
+        response = client.patch.custom_fields(id=field_id, display_order=new_position, fields='all')
+        
+        logging.info(json.dumps(response, indent=2))
+        return response
     
+    except Exception as e:
+        logging.debug(f"An error occurred: {e}")
+          
 def get_custom_fields(client:Client=None, parent_type="matter"):
     try:
         response = {"Success": True}
